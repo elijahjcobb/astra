ff8ee5b3c1de7003c4c0a1e3348e0803b3494a6d
Elijah Cobb <ejcobb@mtu.edu>
2021-05-12 16:17:47 -0400

    changed options

--- src/TestBed.tsx
+++ src/TestBed.tsx
@@ -6,19 +6,26 @@
 
 import {ReactElement, useState} from "react";
 import "./TestBed.css";
-import {PUIApp, PUIColor} from "./helpers/PUIApp";
-import {PUIAlert} from "./components/PUIAlert/PUIAlert";
+import {PUIApp} from "./helpers/PUIApp";
+import {PUIImmutableNumericStatus} from "./components/PUIImmutableNumericStatus/PUIImmutableNumericStatus";
+import {PUISegmentedPicker} from "./components/PUISegmentedPicker/PUISegmentedPicker";
+import {PUIMutableBinaryStatus} from "./components/PUIMutableBinaryStatus/PUIMutableBinaryStatus";
+import {PUILog} from "./components/PUILog/PUILog";
+import {PUIImmutableBinaryStatus} from "./components/PUIImmutableBinaryStatus/PUIImmutableBinaryStatus";
+import {PUIPicker} from "./components/PUIPicker/PUIPicker";
+import {PUIGage} from "./components/PUIGage/PUIGage";
+import {PUIMutableNumericStatus} from "./components/PUIMutableNumericStatus/PUIMutableNumericStatus";
 
 
 export function TestBed(): ReactElement {
 
-	// const [value, setValue] = useState(0);
-	// const [mode, setMode] = useState(0);
-	// const [boo, setBoo] = useState(false);
-	//
-	// const currentRange: [number, number] = [0, 1];
-	// const currentPrecision = 2;
-	//
+	const [value, setValue] = useState(0);
+	const [mode, setMode] = useState(0);
+	const [boo, setBoo] = useState(false);
+
+	const currentRange: [number, number] = [0, 1];
+	const currentPrecision = 2;
+
 
 	return <PUIApp className={"TestBed"}>
 		<span onClick={() => {
@@ -27,40 +34,40 @@ export function TestBed(): ReactElement {
 				message: "This is the message. It is smaller and longer."
 			})
 		}}>open</span>
-		{/*<PUIMutableNumericStatus label={"Current 1"} range={currentRange} precision={currentPrecision} value={value} setValue={(v) => {*/}
-		{/*	console.log(v)*/}
-		{/*	setValue(v)*/}
-		{/*}}/>*/}
-		{/*<PUIImmutableNumericStatus label={"Current 1"} precision={currentPrecision} range={currentRange} value={value}/>*/}
-		{/*<PUISegmentedPicker*/}
-		{/*	label={"Mode"}*/}
-		{/*	options={["A", "B", "C", "D", "E", "F", "G", "H"]}*/}
-		{/*	value={mode}*/}
-		{/*	setValue={setMode}*/}
-		{/*/>*/}
-		{/*<PUIPicker*/}
-		{/*	label={"Mode"}*/}
-		{/*	options={["A", "B", "C", "D", "E", "F", "G", "H"]}*/}
-		{/*	value={mode}*/}
-		{/*	setValue={setMode}*/}
-		{/*/>*/}
-		{/*<PUIImmutableBinaryStatus label={"Boo"} value={boo}/>*/}
-		{/*<PUIMutableBinaryStatus label={"Boo"} value={boo} setValue={setBoo}/>*/}
-		{/*<span onClick={() => {*/}
-		{/*	PUIApp.shared().logInfo("Well this is an info.");*/}
-		{/*}}>Add Info</span>*/}
-		{/*<span onClick={() => {*/}
-		{/*	PUIApp.shared().logWarning("Well this is an info.");*/}
-		{/*}}>Add Warning</span>*/}
-		{/*<span onClick={() => {*/}
-		{/*	PUIApp.shared().logError("Well this is an info.");*/}
-		{/*}}>Add Error</span>*/}
-		{/*<span onClick={() => {*/}
-		{/*	PUIApp.shared().logDebug("Well this is an info.");*/}
-		{/*}}>Add Debug</span>*/}
-		{/*<PUIGage label={"Current 1"} unit={"amps"} value={value} range={currentRange} precision={currentPrecision}/>*/}
-		{/*<PUILog />*/}
-		{/*<PUILog />*/}
+		<PUIMutableNumericStatus label={"Current 1"} range={currentRange} precision={currentPrecision} value={value} setValue={(v) => {
+			console.log(v)
+			setValue(v)
+		}}/>
+		<PUIImmutableNumericStatus label={"Current 1"} precision={currentPrecision} range={currentRange} value={value}/>
+		<PUISegmentedPicker
+			label={"Mode"}
+			options={["A", "B", "C", "D", "E", "F", "G", "H"]}
+			value={mode}
+			setValue={setMode}
+		/>
+		<PUIPicker
+			label={"Mode"}
+			options={["A", "B", "C", "D", "E", "F", "G", "H"]}
+			value={mode}
+			setValue={setMode}
+		/>
+		<PUIImmutableBinaryStatus label={"Boo"} value={boo}/>
+		<PUIMutableBinaryStatus label={"Boo"} value={boo} setValue={setBoo}/>
+		<span onClick={() => {
+			PUIApp.shared().logInfo("Well this is an info.");
+		}}>Add Info</span>
+		<span onClick={() => {
+			PUIApp.shared().logWarning("Well this is an info.");
+		}}>Add Warning</span>
+		<span onClick={() => {
+			PUIApp.shared().logError("Well this is an info.");
+		}}>Add Error</span>
+		<span onClick={() => {
+			PUIApp.shared().logDebug("Well this is an info.");
+		}}>Add Debug</span>
+		<PUIGage label={"Current 1"} unit={"amps"} value={value} range={currentRange} precision={currentPrecision}/>
+		<PUILog />
+		<PUILog />
 	</PUIApp>
 
 }
---
+++ src/components/PUIAlert/PUIAlert.tsx
@@ -0,0 +1,43 @@
+/*
+ * Elijah Cobb
+ * github.com/elijahjcobb
+ * ejcobb@mtu.edu
+ */
+
+import {PropsWithChildren, ReactElement} from "react";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@material-ui/core";
+import {PUIColor} from "../../helpers/PUIApp";
+
+export interface PUIAlertConfig {
+	title?: string;
+	message?: string;
+	actions?: {text: string, color?: PUIColor, onClick?: () => void}[]
+}
+
+export interface PUIAlertProps {
+	config: PUIAlertConfig;
+	open: boolean;
+	onClose: () => void;
+}
+
+export function PUIAlert(props: PropsWithChildren<PUIAlertProps>): ReactElement {
+
+	return (<Dialog open={props.open} onClose={props.onClose}>
+		<DialogTitle>{props.config.title}</DialogTitle>
+		<DialogContent>
+			<DialogContentText>{props.config.message}</DialogContentText>
+		</DialogContent>
+		{props.config.actions && (<DialogActions>
+			{
+				props.config.actions.map((a, i) => {
+					return <Button
+						key={i}
+						style={{color: a.color ?? PUIColor.blue}}
+						onClick={a.onClick}
+					>{a.text}</Button>
+				})
+			}
+		</DialogActions>)}
+	</Dialog>)
+
+}
